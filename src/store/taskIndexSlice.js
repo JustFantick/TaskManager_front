6cf3812cef9272e027339342b73aefd514618b5f
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-04-18 21:29:56 +0300

    Changed counterReducer to taskIndexReducer

---
+++ src/store/taskIndexSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const taskIndexSlice = createSlice({
+	name: 'taskIndex',
+	initialState: {
+		value: 0,
+	},
+	reducers: {
+		setTaskIndex: (state, newIndex) => {
+			state.value += newIndex.payload;
+		},
+	},
+});
+
+// Action creators are generated for each case reducer function
+export const { setTaskIndex } = taskIndexSlice.actions;
+
+export default taskIndexSlice.reducer;
