7ffca3fc8caeb1595af7347ae15cff6bea800ec3
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-04-19 19:28:46 +0300

    Put out some querySelectors as global funct`s vars

--- src/components/popup/popup.jsx
+++ src/components/popup/popup.jsx
@@ -4,16 +4,24 @@ import { useSelector, useDispatch } from 'react-redux';
 import { removeTask } from '../../store/tasksSlice.js';
 
 export default function DeleteTaskPopup() {
+	const dispatch = useDispatch();
+	const currentTask = useSelector((state) => state.tasks[currentTaskIndex]);
+	const currentTaskIndex = useSelector((state) => state.taskIndex.value);
+
+	const popupWrapper = document.querySelector('.popup');
+
 	function hidePopup() {
-		document.querySelector('.popup').classList.remove('active');
+		popupWrapper.classList.remove('active');
 	}
+
 	function outBodyClickHandler(e) {
 		if (!e.target.closest('.popup__body')) hidePopup();
 	}
 
 	function deleteBtnClickHandler() {
+		popupWrapper.classList.remove('active');
+
 		document.querySelector('.wrapper').classList.remove('active');
-		document.querySelector('.popup').classList.remove('active');
 		document.querySelectorAll('.task').forEach((task) => {
 			task.classList.remove('active');
 		});
@@ -21,18 +29,11 @@ export default function DeleteTaskPopup() {
 		dispatch(removeTask(currentTaskIndex));
 	}
 
-	const currentTaskIndex = useSelector((state) => state.taskIndex.value);
-	const currentTask = useSelector((state) => state.tasks[currentTaskIndex]);
-	const dispatch = useDispatch();
-
 	return (
 		<div className='popup' onClick={outBodyClickHandler}>
 			<div className="popup__body">
 				<div className='popup__question'>
-					You want to delete the '{
-						currentTask ?
-							currentTask.title : ''
-					}', right?
+					You want to delete the '{currentTask ? currentTask.title : ''}'a, right?
 				</div>
 				<button onClick={hidePopup} className='popup__cancel-btn'>Cancel</button>
 				<button onClick={deleteBtnClickHandler} className='popup__right-btn'>Delete</button>
