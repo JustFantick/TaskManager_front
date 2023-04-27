32d3301f6503a4a6e17b265a6a81f51f4977bf58
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-04-27 20:16:37 +0300

    Removed unused code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import background from "../img/bg1.webp"
 import Main from "./main/main.jsx";
 import Sidebar from "./sidebar/sidebar.jsx";
-import DeleteTaskPopup from './popup/popup.jsx';
 
 export default function App() {
-	function updateEditingTime() {
-		setTasks(tasks.map((task, i) => {
-			if (taskIndex == i) {
-				task.lastEdit = new Date().toLocaleString('ru', {
-					hour: 'numeric',
-					minute: 'numeric',
-				});
-			}
-			return task;
-		}));
-	}
-
 	return (
 		<div className="wrapper" style={{ backgroundImage: `url(${background})` }}>
 			<Main />
