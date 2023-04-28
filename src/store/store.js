e841ce516dd32cc73b90895d676ae98ccbdb58f6
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-04-28 20:29:38 +0300

    Added new store item

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import taskIndexReducer from './taskIndexSlice.js'
 import tasksReducer from "./tasksSlice.js";
+import authorizationStatusReducer from "./authorizationStatus.js";
 
 export default configureStore({
 	reducer: {
+		authorizationStatus: authorizationStatusReducer,
 		tasks: tasksReducer,
 		taskIndex: taskIndexReducer,
 	},
