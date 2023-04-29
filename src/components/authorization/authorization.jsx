8d0f40286b6d464a9bbdedd57310264adc3a4175
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-04-29 19:52:06 +0300

    Added validation & component animation

--- src/components/authorization/authorization.jsx
+++ src/components/authorization/authorization.jsx
@@ -1,54 +1,120 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Button from '../button/button.jsx';
 import arrow from '../../img/side-arrow.svg';
 import Preloader from '../preloader/preloader.jsx';
 
+import { useDispatch } from 'react-redux';
+import { setAuthorized } from '../../store/authorizationStatus.js';
+import { CSSTransition } from 'react-transition-group';
+
 export default function Authorization() {
+	const dispatch = useDispatch();
+
+	const [authorizationInAnim, setAuthorizationInAnim] = useState(false);
 	const [showLoader, setShowLoader] = useState(false);
 
-	function authorizeClickhandler() {
-		setShowLoader(true);
+	useEffect(() => {
+		setAuthorizationInAnim(true);
+	}, []);
 
-		setTimeout(() => {
-			setShowLoader(false);
-		}, 3000);
+	const [emailValidStatus, setEmailValidStatus] = useState('');
+	const [passwordValidStatus, setPasswordValidStatus] = useState('');
+
+	const [loginlInput, passwordInput, loginlLabel, passwordLabel] = [
+		useRef(null), useRef(null), useRef(null), useRef(null),
+	];
+
+	function validateLogin(currentInputValue) {
+		if (currentInputValue === '') {
+			console.log('empty');
+			setEmailValidStatus('non-valid');
+		} else if (/\s/.test(currentInputValue)) {
+			console.log('input shouldn`t got any white spaces');
+			setEmailValidStatus('non-valid');
+		} else if (/^\d+$/.test(currentInputValue)) {
+			console.log('str contains only numbers');
+			setEmailValidStatus('non-valid');
+		} else {
+			setEmailValidStatus('valid');
+			console.log('valid');
+		}
+	}
+
+	function validatePassword(currentInputValue) {
+		if (currentInputValue === '') {
+			setPasswordValidStatus('non-valid');
+		} else if (currentInputValue.length < 4) {
+			setPasswordValidStatus('non-valid');
+		} else {
+			setPasswordValidStatus('valid');
+		}
+	}
+
+	function authorizeClickhandler() {
+		if (emailValidStatus === 'valid' && passwordValidStatus === 'valid') {
+			setShowLoader(true);
+
+			setTimeout(() => {
+				setShowLoader(false);
+				setAuthorizationInAnim(false);
+
+				//500ms for close-anim of this component
+				setTimeout(() => dispatch(setAuthorized()), 500);
+			}, 1500);
+		} else {
+			validateLogin(loginlInput.current.value);
+			validatePassword(passwordInput.current.value);
+		}
 	}
 
 	return (
 		<React.Fragment>
-			<div className='authorization'>
-				<div className="authorization__body">
-					<div className="authorization__title">Authorization</div>
-					<div className="authorization__line"></div>
+			<CSSTransition
+				in={authorizationInAnim}
+				timeout={500}
+				classNames='authorization'
+				mountOnEnter unmountOnExit
+			>
+				<div className='authorization'>
+					<div className="authorization__body">
+						<div className="authorization__title">Authorization</div>
+						<div className="authorization__line"></div>
 
 
-					<div className="authorization__inputs">
-						<div className="input-group">
-							<label htmlFor='email-input' className="input-group__label">Enter your login</label>
-							<input placeholder='email@gmail.com' type='email' id='email-input' className="input-group__input" />
+						<div className="authorization__inputs">
+							<div className="input-group">
+								<label htmlFor='email-input' ref={loginlLabel} className="input-group__label">Enter your login</label>
+								<input placeholder='email@gmail.com' type='email'
+									ref={loginlInput}
+									onBlur={(e) => validateLogin(e.target.value)}
+									id='email-input' className={`input-group__input ${emailValidStatus}`} />
+							</div>
+
+							<div className="input-group">
+								<label htmlFor='password-input' ref={passwordLabel} className="input-group__label">Enter your password</label>
+								<input type='password' placeholder='_____'
+									ref={passwordInput}
+									onBlur={(e) => validatePassword(e.target.value)}
+									id='password-input' className={`input-group__input ${passwordValidStatus}`} />
+							</div>
+
 						</div>
 
-						<div className="input-group">
-							<label htmlFor='password-input' className="input-group__label">Enter your password</label>
-							<input type='password' id='password-input' className="input-group__input" />
+						<div className="authorization__buttons">
+							<Button onClickHandler={() => console.log('Register')}>
+								Register & log in
+							</Button>
+
+							<Button onClickHandler={authorizeClickhandler}>
+								Authorize <img src={arrow} alt="arrow" />
+							</Button>
+
 						</div>
-
-					</div>
-
-					<div className="authorization__buttons">
-						<Button onClickHandler={() => console.log('Register')}>
-							Register & log in
-						</Button>
-
-						<Button onClickHandler={authorizeClickhandler}>
-							Authorize <img src={arrow} alt="arrow" />
-						</Button>
 
 					</div>
 
 				</div>
-
-			</div>
+			</CSSTransition>
 
 			<Preloader inAnim={showLoader} />
 		</React.Fragment>
