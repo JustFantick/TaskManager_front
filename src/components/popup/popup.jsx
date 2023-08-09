79705ca29948f32b11a27336eddbfccb2e64b742
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-08-09 19:31:07 +0300

    DeleteTaskPopup rewritten as inheritor of Popup,

--- src/components/popup/popup.jsx
+++ src/components/popup/popup.jsx
@@ -1,27 +1,34 @@
 import React from 'react';
-
 import { useSelector, useDispatch } from 'react-redux';
 import { removeTask } from '../../store/tasksSlice.js';
-
 import { CSSTransition } from 'react-transition-group';
 
-export default function DeleteTaskPopup(props) {
-	const dispatch = useDispatch();
-	const currentTaskIndex = useSelector((state) => state.taskIndex.value);
-	const currentTask = useSelector((state) => state.tasks[currentTaskIndex]);
-
-
-	function hidePopup() {
-		props.disactivatePopup();
-	}
-
+export default function Popup({ isPopupOpen, hidePopup, children }) {
 	function outBodyClickHandler(e) {
 		if (!e.target.closest('.popup__body')) hidePopup();
 	}
 
+	return (
+		<CSSTransition
+			in={isPopupOpen} timeout={300} classNames={'popup'}
+			mountOnEnter unmountOnExit>
+			<div className='popup' onClick={outBodyClickHandler}>
+				<div className="popup__body">
+					{children}
+				</div>
+			</div>
+		</CSSTransition>
+	)
+}
+
+export function DeleteTaskPopup({ isPopupOpen, hidePopup }) {
+	const dispatch = useDispatch();
 	const userId = useSelector((state) => state.authorizationData.userId);
+	const currentTaskIndex = useSelector((state) => state.taskIndex.value);
+	const currentTask = useSelector((state) => state.tasks[currentTaskIndex]);
+
 	function deleteBtnClickHandler() {
-		props.disactivatePopup();
+		hidePopup();
 
 		document.querySelector('.wrapper').classList.remove('active');
 		document.querySelectorAll('.task').forEach((task) => {
@@ -36,18 +43,12 @@ export default function DeleteTaskPopup(props) {
 	}
 
 	return (
-		<CSSTransition
-			in={props.popupOpen} timeout={300} classNames={'popup'}
-			mountOnEnter unmountOnExit>
-			<div className='popup' onClick={outBodyClickHandler}>
-				<div className="popup__body">
-					<div className='popup__question'>
-						You want to delete the '{currentTask ? currentTask.title : ''}', right?
-					</div>
-					<button onClick={hidePopup} className='popup__cancel-btn'>Cancel</button>
-					<button onClick={deleteBtnClickHandler} className='popup__right-btn'>Delete</button>
-				</div>
+		<Popup isPopupOpen={isPopupOpen} hidePopup={hidePopup}>
+			<div className='popup__question'>
+				You want to delete the '{currentTask && currentTask.title}', right?
 			</div>
-		</CSSTransition>
+			<button onClick={hidePopup} className='popup__cancel-btn'>Cancel</button>
+			<button onClick={deleteBtnClickHandler} className='popup__right-btn'>Delete</button>
+		</Popup>
 	)
 }
